78fe67bb9c19a16734f1ece9b69db76b21b587da
Will Morgan <will@willmorgan.co.uk>
2018-11-20 21:40:33 +0000

    Add test skeleton

--- test/repro.test.js
+++ test/repro.test.js
@@ -1,3 +1,16 @@
+"use strict";
+
+const mssql = require('mssql');
+
+const connString = 'Data Source=tcp:localhost,1433;Initial Catalog=mssql_test;User Id=sa;Password=Upper_l0wercase;Encrypt=true;';
+
 describe('node-mssql-repro', () => {
+    let connection;
+    before('connect', async () => {
+        connection = await mssql.connect(connString)
+    });
+    after('disconnect', async () => {
+        await connection.close();
+    });
     it('breaks as expected :-)');
 });

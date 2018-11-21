00523c22382c631f8f4f1353f5199620affac1ee
Will Morgan <will@willmorgan.co.uk>
2018-11-21 20:05:32 +0000

    Confirmed broken reproduction

--- test/repro.test.js
+++ test/repro.test.js
@@ -15,5 +15,26 @@ describe('node-mssql-repro', () => {
     after('disconnect', async () => {
         await connection.close();
     });
-    it('breaks as expected :-)');
+    it('breaks as expected :-)', async () => {
+        const statement = `if object_id('"dbo"."T1"','U') is NULL create table "dbo"."T1" ("location" geometry);`
+        await connection.query(statement);
+
+        const ps = new mssql.PreparedStatement(connection);
+        try {
+            ps.input('P1',mssql.Geometry);
+            await ps.prepare(`insert into "dbo"."T1" ("location") values (@P1)`);
+            console.log(await ps.execute({
+                P1: '0 1 2 3',
+            }));
+            await ps.unprepare();
+        } catch (err) {
+            await ps.unprepare();
+            console.error(err);
+            throw err;
+        }
+        finally {
+            await ps.unprepare();
+        }
+
+    });
 });
